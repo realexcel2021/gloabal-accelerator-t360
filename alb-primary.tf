6fc25acd9ca394a99d7cff0980850ebdebe4fbee
realexcel2021 <realexcel2020@gmail.com>
2024-07-10 16:33:15 +0100

    added random

--- alb-primary.tf
+++ alb-primary.tf
@@ -90,3 +90,65 @@ module "alb" {
 
 }
 
+
+module "alb_load" {
+  source = "terraform-aws-modules/alb/aws"
+
+  name    = "t360-load-testing-lb"
+  vpc_id  = module.vpc.vpc_id
+  subnets = module.vpc.public_subnets
+
+  enable_deletion_protection = false
+
+
+
+  # Security Group
+  security_group_ingress_rules = {
+    all_http = {
+      from_port   = 80
+      to_port     = 80
+      ip_protocol = "tcp"
+      description = "HTTP web traffic"
+      cidr_ipv4   = "0.0.0.0/0"
+    }
+  }
+  security_group_egress_rules = {
+    all = {
+      ip_protocol = "-1"
+      cidr_ipv4   = "0.0.0.0/0"
+    }
+  }
+
+  listeners = {
+    ex-http-https = {
+      port     = 80
+      protocol = "HTTP"
+      forward = {
+        target_group_key = "ex-ip"
+      }
+    }
+  }
+
+  target_groups = {
+    ex-ip = {
+      name_prefix               = "l1-"
+      target_type               = "ip"
+      protocol                  = "HTTPS"
+      port                      = 8090
+      create_attachment = false
+      health_check = {
+        enabled             = true
+        interval            = 35
+        port                = 8090
+        healthy_threshold   = 3
+        unhealthy_threshold = 5
+        timeout             = 30
+        protocol            = "HTTPS"
+        matcher             = "403"
+      }
+    }
+  }
+
+}
+
+

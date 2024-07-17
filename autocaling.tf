d76b459a383d0e2de1786028bd7e6793e1c97205
sheriff joe <realexcel2020@gmail.com>
2024-07-17 11:56:10 +0100

    removed asg

--- autocaling.tf
+++ autocaling.tf
@@ -1,17 +1,17 @@
 
-module "ecs_cloudwatch_autoscaling" {
-  source = "cloudposse/ecs-cloudwatch-autoscaling/aws"
-  # Cloud Posse recommends pinning every module to a specific version
-  # version     = "x.x.x"
-  namespace             = "t360-api"
-  stage                 = "staging"
-  name                  = "t360-scaling"
-  service_name          = module.ecs_service_wrk.name
-  cluster_name          = module.ecs_cluster.name
-  min_capacity          = 4
-  max_capacity          = 20
-  scale_up_adjustment   = 2
-  scale_up_cooldown     = 60
-  scale_down_adjustment = -1
-  scale_down_cooldown   = 300
-}
+# module "ecs_cloudwatch_autoscaling" {
+#   source = "cloudposse/ecs-cloudwatch-autoscaling/aws"
+#   # Cloud Posse recommends pinning every module to a specific version
+#   # version     = "x.x.x"
+#   namespace             = "t360-api"
+#   stage                 = "staging"
+#   name                  = "t360-scaling"
+#   service_name          = module.ecs_service_wrk.name
+#   cluster_name          = module.ecs_cluster.name
+#   min_capacity          = 4
+#   max_capacity          = 20
+#   scale_up_adjustment   = 2
+#   scale_up_cooldown     = 60
+#   scale_down_adjustment = -1
+#   scale_down_cooldown   = 300
+# }
